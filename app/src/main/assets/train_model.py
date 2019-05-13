d050142279afdb47d8b9c38083730432bcc9b7d9
felmoreno1726 <felipemoreno1626@gmail.com>
2019-05-13 02:24:59 -0400

    Integrated tensorflow inference predictor. TODO debug labels

---
+++ app/src/main/assets/train_model.py
@@ -0,0 +1,11 @@
+import os
+import keras
+from keras.models import Model
+import tensorflow as tf
+from convertToTensorflow import keras_to_tensorflow
+
+#this should be replaced later by the model
+mobile_model = keras.applications.mobilenet.MobileNet()
+output_dir = os.path.join(os.getcwd(), "output_model")
+keras_to_tensorflow(mobile_model, output_dir=output_dir, model_name="mobile_model.pb")
+
--- app/src/main/java/com/felmoreno1726/imageencyclopedia/MainActivity.java
+++ app/src/main/java/com/felmoreno1726/imageencyclopedia/MainActivity.java
@@ -1,5 +1,8 @@
 package com.felmoreno1726.imageencyclopedia;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +11,96 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    //PATH TO OUR MODEL FILE AND NAMES OF THE INPUT AND OUTPUT NODES
+    private String MODEL_PATH = "file:///android_asset/output_model/mobile_model.pb";
+    private String INPUT_NAME = "input_1";
+    private String OUTPUT_NAME = "output_1";
+    private TensorFlowInferenceInterface tf;
+
+    //ARRAY TO HOLD THE PREDICTIONS AND FLOAT VALUES TO HOLD THE IMAGE DATA
+    float[] PREDICTIONS = new float[1000];
+    private float[] floatValues;
+    private int[] INPUT_SIZE = {224,224,3};
+
+    ImageView imageView;
+    TextView resultView;
+    Snackbar progressBar;
+
+    public Object[] argmax(float[] array) {
+        /**
+         * Computes the maximum predictions of confidence
+         */
+        int best = -1;
+        float best_confidence = 0.0f;
+        for (int i = 0; i < array.length; i++) {
+            float value = array[i];
+            if (value > best_confidence) {
+                best_confidence = value;
+                best = i;
+            }
+        }
+        return new Object[]{best, best_confidence};
+    }
+
+    public void predict(final Bitmap bitmap){
+        //Runs inference in background thread
+        new AsyncTask<Integer,Integer,Integer>(){
+
+            @Override
+            protected Integer doInBackground(Integer ...params){
+                //Resize the image into 224 x 224
+                Bitmap resized_image = ImageUtils.processBitmap(bitmap,224);
+                //Normalize the pixels
+                floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
+                //Pass input into the tensorflow
+                tf.feed(INPUT_NAME,floatValues,1,224,224,3);
+                //compute predictions
+                tf.run(new String[]{OUTPUT_NAME});
+                //copy the output into the PREDICTIONS array
+                tf.fetch(OUTPUT_NAME,PREDICTIONS);
+                System.out.println("Fetched predictions");
+                //Obtained highest prediction
+                Object[] results = argmax(PREDICTIONS);
+                System.out.println("Predictions: " + PREDICTIONS);
+                System.out.println("Results: " + results);
+                int class_index = (Integer) results[0];
+                float confidence = (Float) results[1];
+                try{
+                    final String conf = String.valueOf(confidence * 100).substring(0,5);
+                    System.out.println("Confidence: " + conf);
+                    //Convert predicted class index into actual label name
+                    //TODO
+                    final String label = ImageUtils.getLabel(getAssets().open("labels.json"),class_index);//Needs a json file to go from val to label
+                    System.out.println("Label: " + label);
+                    //Display result on UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.dismiss();
+                            System.out.println("DIsmissed progress bar");
+                            resultView.setText(label + " : " + conf + "%");
+                            System.out.print("should be displaying the label on the textView");
+                        }
+                    });
+                }
+                catch (Exception e){
+                    System.out.println("Error when running prediction.");
+                }
+                return 0;
+            }
+        }.execute(0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +109,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+
+        tf = new TensorFlowInferenceInterface(getAssets(),MODEL_PATH);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        resultView = (TextView) findViewById(R.id.TextViewResult);
+        progressBar = Snackbar.make(imageView,"PROCESSING IMAGE",Snackbar.LENGTH_INDEFINITE);
+        FloatingActionButton fab = findViewById(R.id.fab);//Currently used to initiate a predict
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                try{
+                    //READ THE IMAGE FROM ASSETS FOLDER
+                    InputStream imageStream = getAssets().open("images/dog.jpeg");
+                    Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+                    imageView.setImageBitmap(bitmap);
+                    progressBar.show();
+                    predict(bitmap);
+                }
+                catch (Exception e){
+                    System.out.println("Error predicting image");
+                }
             }
         });
     }
