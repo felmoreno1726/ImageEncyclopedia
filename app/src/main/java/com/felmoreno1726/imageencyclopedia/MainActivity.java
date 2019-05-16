e229199653d86470824157520164837d52a97464
felmoreno1726 <felipemoreno1626@gmail.com>
2019-05-15 23:43:21 -0400

    Integrated takePicture and uploadFromGallery functionalities

--- app/src/main/java/com/felmoreno1726/imageencyclopedia/MainActivity.java
+++ app/src/main/java/com/felmoreno1726/imageencyclopedia/MainActivity.java
@@ -1,21 +1,24 @@
 package com.felmoreno1726.imageencyclopedia;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,9 +38,12 @@ public class MainActivity extends AppCompatActivity {
     private int[] INPUT_SIZE = {224,224,3};
 
     ImageView imageView;
-    ImageView imageViewTest; //remove this
     TextView resultView;
     Snackbar progressBar;
+    FloatingActionButton buttonLoadImage;
+    FloatingActionButton buttonTakePhoto;
+    private static int RESULT_LOAD_IMAGE = 1;
+    private static int RESULT_TAKE_PICTURE = 2;
 
     public Object[] argmax(float[] array) {
         /**
@@ -63,10 +69,6 @@ public class MainActivity extends AppCompatActivity {
             protected Integer doInBackground(Integer ...params){
                 //Resize the image into 224 x 224
                 Bitmap resized_image = ImageUtils.processBitmap(bitmap,224);
-                // remove everything beginning here:
-                imageViewTest = (ImageView) findViewById(R.id.imageView2);
-                imageViewTest.setImageBitmap(resized_image);
-                // remove till here
                 //Normalize the pixels
                 floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
                 //Pass input into the tensorflow
@@ -114,50 +116,91 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        tf = new TensorFlowInferenceInterface(getAssets(),MODEL_PATH);
-
         imageView = (ImageView) findViewById(R.id.imageView);
         resultView = (TextView) findViewById(R.id.TextViewResult);
-        progressBar = Snackbar.make(imageView,"PROCESSING IMAGE",Snackbar.LENGTH_INDEFINITE);
-        FloatingActionButton fab = findViewById(R.id.fab);//Currently used to initiate a predict
-        fab.setOnClickListener(new View.OnClickListener() {
+        progressBar = Snackbar.make(imageView, "PROCESSING IMAGE", Snackbar.LENGTH_INDEFINITE);
+
+        //
+
+        buttonLoadImage = findViewById(R.id.galleryButton);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    //READ THE IMAGE FROM ASSETS FOLDER
-                    InputStream imageStream = getAssets().open("images/dog.jpeg");
-                    Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
-                    imageView.setImageBitmap(bitmap);
-                    progressBar.show();
-                    predict(bitmap);
+                //Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+                //gallery.setType("image/*");
+                //startActivityForResult(gallery, RESULT_LOAD_IMAGE);
+
+                Intent photoPickEvent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(photoPickEvent, RESULT_LOAD_IMAGE);
+                //Get image from activity callback TODO
+
+            }
+        });
+        buttonTakePhoto = findViewById(R.id.photoButton);
+        buttonTakePhoto.setOnClickListener(new View.OnClickListener(){
+        URI output_URI;
+            @Override
+            public void onClick(View view){
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                if (takePhotoIntent.resolveActivity(getPackageManager()) != null){
+                    takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, output_URI);
+                    startActivityForResult(takePhotoIntent, RESULT_TAKE_PICTURE);
                 }
-                catch (Exception e){
-                    System.out.println("Error predicting image");
-                }
+                startActivityForResult(takePhotoIntent, RESULT_TAKE_PICTURE);
+                //finish();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //If load image from
+        Bitmap imageBitmap = null;
+        Uri imageUri = null;
+        if (resultCode == RESULT_OK) {
+            if (requestCode == RESULT_LOAD_IMAGE) {
+                imageUri = data.getData();
+                //Bundle extras = data.getExtras();
+                //Bitmap imageBitmap = (Bitmap) extras.get("data");
+                System.out.println("Image from gallery has been uploaded");
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageURI(imageUri);
+                //Get the bitmap
+                try {
+                    imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == RESULT_TAKE_PICTURE) {
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+                data.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                System.out.println("Image has been captured");
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                System.out.println("This is the bitmap before setting imageview: " + imageBitmap);
+                imageView.setImageBitmap(imageBitmap);
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            System.out.println("Bitmap has been set");
+            //Then run the image on the model
+            tf = new TensorFlowInferenceInterface(getAssets(), MODEL_PATH);
+            try {
+                //Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                //READ THE IMAGE FROM ASSETS FOLDER
+                InputStream imageStream = getAssets().open("images/dog.jpeg");
+                Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+                //imageView.setImageBitmap(bitmap);
+                progressBar.show();
+                //wait(5);
+                System.out.println("This is the bitmap: " + imageBitmap);
+                predict(imageBitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
